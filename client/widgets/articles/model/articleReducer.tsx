b779fd59fe467a0bb5b8f5442781bcd9ef4a4d2b
Nazir09 <nazirazizov9@gmail.com>
2023-06-03 13:57:21 +0300

    remove imports

--- client/widgets/articles/model/articleReducer.tsx
+++ client/widgets/articles/model/articleReducer.tsx
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-import type { PayloadAction } from '@reduxjs/toolkit'
 import * as articleAsyncActions from './articleAsyncActions'
 import { IArticle } from '../interfaces'
 export interface ArticleState {
